4133cb264eae6c992a806eb5ae52c254fd1db506
David Hoover <hooverd@arizona.edu>
2023-03-23 14:29:23 -0500

    Get Honeycomb API key from env.

--- dev/shim.sh
+++ dev/shim.sh
@@ -3,7 +3,7 @@
 export OTEL_METRICS_EXPORTER=none
 export OTEL_TRACES_EXPORTER=otlp
 export OTEL_EXPORTER_OTLP_ENDPOINT="api.honeycomb.io:443"
-export OTEL_EXPORTER_OTLP_HEADERS="x-honeycomb-team=cZX0pecOaRY6AnM4LoPECF"
+export OTEL_EXPORTER_OTLP_HEADERS="x-honeycomb-team=$X_HONEYCOMB_TEAM"
 export OTEL_SERVICE_NAME="dev-django-ddb-sessions"
 export DJANGO_SETTINGS_MODULE="dev.settings"
 export OTEL_LOG_LEVEL="debug"
